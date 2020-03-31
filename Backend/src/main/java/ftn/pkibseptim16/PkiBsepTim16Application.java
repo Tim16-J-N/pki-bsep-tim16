ae580f956a41f10704289700ae5fe1bf32c76936
NikolaBrodic <nikola.brodic97@gmail.com>
2020-03-31 12:28:41 +0200

    Create initial Spring app

---
+++ Backend/src/main/java/ftn/pkibseptim16/PkiBsepTim16Application.java
@@ -0,0 +1,13 @@
+package ftn.pkibseptim16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiBsepTim16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiBsepTim16Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ftn/pkibseptim16/PkiBsepTim16ApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.pkibseptim16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiBsepTim16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
